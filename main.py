b7896ec91809cb569d505c95e8a9a2d049de8dd3
Pittawat Taveekitworachai <11158905+Pittawat2542@users.noreply.github.com>
2023-09-20 15:43:05 +0900

    refactor: move functions to utils
    Signed-off-by: Pittawat Taveekitworachai <11158905+Pittawat2542@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import argparse
 import os
-import json
 import time
 import logging
 
@@ -8,109 +6,11 @@ import openai
 from dotenv import load_dotenv
 from tqdm import tqdm
 
-from src.config import LOGS_FOLDER, STATS_FOLDER, PROMPTS_PATH, ORIGINAL_PROMPT_PATH
-from src.utils import prepare_output_folders
+from src.config import LOGS_FOLDER
+from src.utils import prepare_output_folders, parse_arguments, load_previous_values
 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
-
-
-def parse_arguments():
-    parser = argparse.ArgumentParser(
-        prog='Prompt Optimization with LLMs')
-
-    parser.add_argument("-mt", "--task-model", type=str,
-                        default="gpt-3.5-turbo",
-                        help='The model to be used for the task.',
-                        choices=["gpt-3.5-turbo", "gpt-4"], )
-    parser.add_argument("-mm", "--modifying-model", type=str,
-                        default="gpt-3.5-turbo",
-                        help='The model to be used for modifying the prompt.',
-                        choices=["gpt-3.5-turbo", "gpt-4"], )
-    parser.add_argument("-p", "--patience", type=int,
-                        help='The number of rounds to wait before stopping the optimization.')
-    parser.add_argument("-t", "--threshold", type=float,
-                        help='The accuracy threshold to stop the optimization.')
-    parser.add_argument("-n", "--n", type=int,
-                        help='The number of evolution round to run. (Either "number of evolution" or (threshold and '
-                             'patience) must be specified.)')
-
-    _args = parser.parse_args()
-
-    if _args.n is None and (_args.threshold is None or _args.patience is None):
-        raise ValueError("Either the number of evolution round or the threshold and patience must be specified.")
-
-    if _args.n is not None and (_args.threshold is not None or _args.patience is not None):
-        raise ValueError("Either the number of evolution round or the threshold and patience must be specified.")
-
-    if _args.n is not None and _args.n <= 0:
-        raise ValueError("The number of evolution round must be greater than 0.")
-
-    if _args.threshold is not None and _args.threshold <= 0:
-        raise ValueError("The threshold must be greater than 0.")
-
-    if _args.patience is not None and _args.patience <= 0:
-        raise ValueError("The patience must be greater than 0.")
-
-    return _args
-
-
-def load_previous_values():
-    last_accuracy = 0
-    last_prompt_version = 0
-    last_round = -1
-
-    if os.path.exists(f'{STATS_FOLDER}/stat.json'):
-        stat_obj = json.load(open(f'{STATS_FOLDER}/stat.json', 'r'))
-        last_accuracy = stat_obj['last_accuracy']
-        last_prompt_version = stat_obj['last_prompt_version']
-
-        if stat_obj.get('patience', None) is not None:
-            args.patience = stat_obj['patience']
-        if stat_obj.get('threshold', None) is not None:
-            args.threshold = stat_obj['threshold']
-        if stat_obj.get('n', None) is not None:
-            args.threshold = stat_obj['n']
-            last_round = stat_obj['last_round']
-        args.task_model = stat_obj['task_model']
-        args.modifying_model = stat_obj['modifying_model']
-
-        with open(PROMPTS_PATH, 'r') as f:
-            prompts = json.load(f)["prompts"]
-            last_prompt = [p for p in prompts if p["version"] == last_prompt_version][0]["prompt"]
-    else:
-        with open(f'{STATS_FOLDER}/stat.json', 'w') as f:
-            stat_obj = {
-                "last_accuracy": last_accuracy,
-                "best_accuracy": last_accuracy,
-                "last_prompt_version": last_prompt_version,
-                "best_prompt_version": last_prompt_version,
-                "task_model": args.task_model,
-                "modifying_model": args.modifying_model,
-            }
-
-            if args.threshold is not None and args.patience is not None:
-                stat_obj["threshold"] = args.threshold
-                stat_obj["patience"] = args.patience
-            else:
-                stat_obj["n"] = args.n
-                last_round = 0
-                stat_obj["last_round"] = last_round
-
-            f.write(json.dumps(stat_obj))
-
-            last_prompt = open(ORIGINAL_PROMPT_PATH, 'r').read()
-
-            with open(PROMPTS_PATH, 'w') as f2:
-                f2.write(json.dumps({
-                    "prompts": [{
-                        "version": last_prompt_version,
-                        "prompt": last_prompt
-                    }]
-                }))
-
-    return last_accuracy, last_prompt_version, last_prompt, last_round
-
 
 if __name__ == "__main__":
     prepare_output_folders()
@@ -121,8 +21,7 @@ if __name__ == "__main__":
                         filemode='a', datefmt='%Y-%m-%d %H:%M:%S')
 
     args = parse_arguments()
-
-    current_accuracy, prompt_version, prompt, current_round = load_previous_values()
+    current_accuracy, prompt_version, prompt, current_round = load_previous_values(args)
 
     if args.n is not None and current_round != -1:
         for i in tqdm(range(args.n - current_round)):
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import os
 import time
@@ -5,7 +6,7 @@ import time
 import pandas as pd
 
 from src.config import OUTPUTS_FOLDER, LOGS_FOLDER, STATS_FOLDER, PARSED_FOLDER, RAW_FOLDER, EVALUATION_FOLDER, \
-    DATASET_PATH
+    DATASET_PATH, PROMPTS_PATH, ORIGINAL_PROMPT_PATH
 
 
 def sleep(seconds: int) -> None:
@@ -58,3 +59,102 @@ def prepare_dataset():
         'identity_hate')
 
     return df
+
+
+def parse_arguments():
+    """Parse the arguments passed to the program."""
+    parser = argparse.ArgumentParser(
+        prog='Prompt Optimization with LLMs')
+
+    parser.add_argument("-mt", "--task-model", type=str,
+                        default="gpt-3.5-turbo",
+                        help='The model to be used for the task.',
+                        choices=["gpt-3.5-turbo", "gpt-4"], )
+    parser.add_argument("-mm", "--modifying-model", type=str,
+                        default="gpt-3.5-turbo",
+                        help='The model to be used for modifying the prompt.',
+                        choices=["gpt-3.5-turbo", "gpt-4"], )
+    parser.add_argument("-p", "--patience", type=int,
+                        help='The number of rounds to wait before stopping the optimization.')
+    parser.add_argument("-t", "--threshold", type=float,
+                        help='The accuracy threshold to stop the optimization.')
+    parser.add_argument("-n", "--n", type=int,
+                        help='The number of evolution round to run. (Either "number of evolution" or (threshold and '
+                             'patience) must be specified.)')
+
+    _args = parser.parse_args()
+
+    if _args.n is None and (_args.threshold is None or _args.patience is None):
+        raise ValueError("Either the number of evolution round or the threshold and patience must be specified.")
+
+    if _args.n is not None and (_args.threshold is not None or _args.patience is not None):
+        raise ValueError("Either the number of evolution round or the threshold and patience must be specified.")
+
+    if _args.n is not None and _args.n <= 0:
+        raise ValueError("The number of evolution round must be greater than 0.")
+
+    if _args.threshold is not None and _args.threshold <= 0:
+        raise ValueError("The threshold must be greater than 0.")
+
+    if _args.patience is not None and _args.patience <= 0:
+        raise ValueError("The patience must be greater than 0.")
+
+    return _args
+
+
+def load_previous_values(args):
+    """Load the previous values from the stat.json file."""
+    last_accuracy = 0
+    last_prompt_version = 0
+    last_round = -1
+
+    if os.path.exists(f'{STATS_FOLDER}/stat.json'):
+        stat_obj = json.load(open(f'{STATS_FOLDER}/stat.json', 'r'))
+        last_accuracy = stat_obj['last_accuracy']
+        last_prompt_version = stat_obj['last_prompt_version']
+
+        if stat_obj.get('patience', None) is not None:
+            args.patience = stat_obj['patience']
+        if stat_obj.get('threshold', None) is not None:
+            args.threshold = stat_obj['threshold']
+        if stat_obj.get('n', None) is not None:
+            args.threshold = stat_obj['n']
+            last_round = stat_obj['last_round']
+        args.task_model = stat_obj['task_model']
+        args.modifying_model = stat_obj['modifying_model']
+
+        with open(PROMPTS_PATH, 'r') as f:
+            prompts = json.load(f)["prompts"]
+            last_prompt = [p for p in prompts if p["version"] == last_prompt_version][0]["prompt"]
+    else:
+        with open(f'{STATS_FOLDER}/stat.json', 'w') as f:
+            stat_obj = {
+                "last_accuracy": last_accuracy,
+                "best_accuracy": last_accuracy,
+                "last_prompt_version": last_prompt_version,
+                "best_prompt_version": last_prompt_version,
+                "task_model": args.task_model,
+                "modifying_model": args.modifying_model,
+            }
+
+            if args.threshold is not None and args.patience is not None:
+                stat_obj["threshold"] = args.threshold
+                stat_obj["patience"] = args.patience
+            else:
+                stat_obj["n"] = args.n
+                last_round = 0
+                stat_obj["last_round"] = last_round
+
+            f.write(json.dumps(stat_obj))
+
+            last_prompt = open(ORIGINAL_PROMPT_PATH, 'r').read()
+
+            with open(PROMPTS_PATH, 'w') as f2:
+                f2.write(json.dumps({
+                    "prompts": [{
+                        "version": last_prompt_version,
+                        "prompt": last_prompt
+                    }]
+                }))
+
+    return last_accuracy, last_prompt_version, last_prompt, last_round
